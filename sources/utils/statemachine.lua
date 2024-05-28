b266c55161de1498e130007ee7f20a04355db5bd
Kappa <iwaskappa@gmail.com>
2024-05-28 14:29:47 +0800

    Update statemachine.lua

--- sources/utils/statemachine.lua
+++ sources/utils/statemachine.lua
@@ -6,6 +6,10 @@ local function assert(condition, message)
 
 end
 
+local function default()
+    
+end
+
 
 ----------------------------------------------------------------
 local StateMachine = {}
@@ -16,12 +20,8 @@ StateMachine.__index = StateMachine
 function StateMachine:new()
 
     local class = {}
-    class.active = nil
     class.states = {}
-
-    -- initialize default state
-    class.states.idle.enter = function() print("idled") end
-    class.states.idle.update = function() print("idle updates!") end
+    class.active = "idle"
 
     setmetatable(class, self)
     return class
@@ -30,11 +30,13 @@ end
 
 
 ----------------------------------------------------------------
-function StateMachine:create(name, callback)
+function StateMachine:create(name, enterCallback, updateCallback)
 
-    assert(self.states[name] ~= nil, "duplicate states name of " .. name)
+    assert(self.states[name] == nil, "duplicate states name of " .. name)
 
-    self.states[name] = callback
+    self.states[name] = {}
+    self.states[name][1] = enterCallback or default
+    self.states[name][2] = updateCallback or default
 
 end
 
@@ -42,8 +44,8 @@ end
 ----------------------------------------------------------------
 function StateMachine:change(name)
 
-    self.states.active = self.states[name]
-    self.states.active.enter()
+    self.active = name
+    self.states[self.active][1]()
 
 end
 
@@ -51,7 +53,7 @@ end
 ----------------------------------------------------------------
 function StateMachine:update(deltaTime)
 
-    self.states.active.update(deltaTime)
+    self.states[self.active][2](deltaTime)
 
 end
 
